c7399c5fedd46e56dfce5e04432051067fa7f90a
wvalverder <wvalverder@gmail.com>
2023-12-03 11:29:06 -0500

    Second commit

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,37 @@ interface Task {
 }
 
 class taskManager<T extends Task> {
-    private tasks: T[] = [];
-
-    addTask(task: T) {
-        this.tasks.push(task);
+    private task: T[] = [];
+    
+    addTask(description:string):void {
+        const newTask: T = {
+            id: this.task.length + 1,
+            description,
+            completed: false,
+        } as T;
+        this.task.push(newTask);
+        this.displayTask();
     }
 
-    getTasks() {
-        return this.tasks;
+    displayTask(): void {
+        const taskListElement = document.getElementById("task-list");
+        if(taskListElement) {
+            taskListElement.innerHTML = "";
+            this.task.forEach((task) => {
+                const listItem = document.createElement("li");
+                listItem.textContent = `${task.id}.${task.description}.${task.completed ? "Completada" : "Pendiente"}`;
+                taskListElement.appendChild(listItem);
+            })
+        }
+    }
+}
+
+const TaskManager = new taskManager<Task>();
+
+function addTask():void {
+    const taskInput = document.getElementById("task-input") as HTMLInputElement;
+    if(taskInput && taskInput.value.trim() !== "") {
+        TaskManager.addTask(taskInput.value.trim());
+        taskInput.value = "";
     }
 }
